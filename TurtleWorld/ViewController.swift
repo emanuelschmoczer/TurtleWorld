5c2b2e4f19c52c1632010733980f8a6ae305cbba
Emanuel Schmoczer <emanuel@schmoczer.com>
2019-03-12 11:54:43 +0100

    Initial Commit

---
+++ TurtleWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TurtleWorld
+//
+//  Created by Emanuel Schmoczer on 12.03.19.
+//  Copyright © 2019 Emanuel Schmoczer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
